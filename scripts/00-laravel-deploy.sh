04b593d0acc087ca06664ffe09d3bad55008b403
Rustem <36151642+rustemib@users.noreply.github.com>
2023-02-17 22:25:50 +0300

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -11,3 +11,9 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
+
+echo "Running npm install..."
+php npm install --force
+
+echo "Running npm..."
+php npm run dev --force
